679bc6b864216e043047e338a5ac59a41a60290e
Ivan Tsenilov <arantirex@gmail.com>
2024-10-21 19:47:55 +0400

    refactor: user controller

--- src/userController.ts
+++ src/userController.ts
@@ -17,68 +17,107 @@ export const handleRequest = (
   const parsedUrl = parse(url || '', true);
   const pathSegments = parsedUrl.pathname?.split('/').filter(Boolean);
 
-  console.log('parsedUrl', parsedUrl.pathname?.split('/'));
-
+  // Handle non-existent routes
   if (
-    pathSegments &&
-    pathSegments[0] === 'api' &&
-    pathSegments[1] === 'users'
+    !(pathSegments && pathSegments[0] === 'api' && pathSegments[1] === 'users')
   ) {
-    const userId = pathSegments[2];
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+    return;
+  }
 
-    try {
-      if (method === 'GET' && pathSegments.length === 2) {
-        // Get all users
-        const users = getAllUsers();
+  const handleInternalError = () => {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
+  };
+
+  const handleRequestError = (message: string) => {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end(message);
+  };
+
+  const userId = pathSegments[2];
+
+  try {
+    // Handle Get users
+    if (method === 'GET' && pathSegments.length === 2) {
+      const users = getAllUsers();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(users));
+      return;
+    }
+
+    // Handle Get user by ID
+    if (method === 'GET' && pathSegments.length === 3) {
+      if (!validateUuid(userId!)) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid user ID');
+        return;
+      }
+
+      const user = getUserById(userId!);
+
+      if (user) {
         res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
-      } else if (method === 'GET' && pathSegments.length === 3) {
-        // Get user by ID
-        if (!validateUuid(userId!)) {
-          res.writeHead(400, { 'Content-Type': 'text/plain' });
-          res.end('Invalid user ID');
-          return;
-        }
-        const user = getUserById(userId!);
-        if (user) {
-          res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify(user));
-        } else {
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('User not found');
-        }
-      } else if (method === 'POST' && pathSegments.length === 2) {
-        // Create a new user
-        let body = '';
-        req.on('data', (chunk) => {
-          body += chunk.toString();
-        });
-        req.on('end', () => {
+        res.end(JSON.stringify(user));
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('User not found');
+      }
+
+      return;
+    }
+
+    // Handle Create new user
+    if (method === 'POST' && pathSegments.length === 2) {
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+        try {
+          if (!body) return handleRequestError('Invalid request body');
+
           const { username, age, hobbies } = JSON.parse(body);
-          if (username && typeof age === 'number' && Array.isArray(hobbies)) {
-            const newUser = createUser(username, age, hobbies);
-            res.writeHead(201, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(newUser));
-          } else {
-            res.writeHead(400, { 'Content-Type': 'text/plain' });
-            res.end('Invalid request body');
-          }
-        });
-      } else if (method === 'PUT' && pathSegments.length === 3) {
-        // Update an existing user
-        if (!validateUuid(userId!)) {
-          res.writeHead(400, { 'Content-Type': 'text/plain' });
-          res.end('Invalid user ID');
-          return;
+
+          if (!(username && typeof age === 'number' && Array.isArray(hobbies)))
+            return handleRequestError('Invalid request body');
+
+          const newUser = createUser(username, age, hobbies);
+
+          res.writeHead(201, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(newUser));
+        } catch (error) {
+          handleInternalError();
         }
-        let body = '';
-        req.on('data', (chunk) => {
-          body += chunk.toString();
-        });
-        req.on('end', () => {
+      });
+
+      return;
+    }
+
+    // Handle Update existing user
+    if (method === 'PUT' && pathSegments.length === 3) {
+      if (!validateUuid(userId!)) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid user ID');
+        return;
+      }
+
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+        try {
+          if (!body) return handleRequestError('Invalid request body');
+
           const { username, age, hobbies } = JSON.parse(body);
+
           if (username && typeof age === 'number' && Array.isArray(hobbies)) {
             const updatedUser = updateUser(userId!, username, age, hobbies);
+
             if (updatedUser) {
               res.writeHead(200, { 'Content-Type': 'application/json' });
               res.end(JSON.stringify(updatedUser));
@@ -87,35 +126,37 @@ export const handleRequest = (
               res.end('User not found');
             }
           } else {
-            res.writeHead(400, { 'Content-Type': 'text/plain' });
-            res.end('Invalid request body');
+            handleRequestError('Invalid request body');
           }
-        });
-      } else if (method === 'DELETE' && pathSegments.length === 3) {
-        // Delete a user
-        if (!validateUuid(userId!)) {
-          res.writeHead(400, { 'Content-Type': 'text/plain' });
-          res.end('Invalid user ID');
-          return;
+        } catch (error) {
+          handleInternalError();
         }
-        const isDeleted = deleteUser(userId!);
-        if (isDeleted) {
-          res.writeHead(204);
-          res.end();
-        } else {
-          res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end('User not found');
-        }
+      });
+
+      return;
+    }
+
+    // Handle Delete a user
+    if (method === 'DELETE' && pathSegments.length === 3) {
+      if (!validateUuid(userId!)) {
+        handleRequestError('Invalid user ID');
+        return;
+      }
+
+      const isDeleted = deleteUser(userId!);
+
+      if (isDeleted) {
+        res.writeHead(204);
+        res.end();
       } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
+        res.end('User not found');
       }
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error');
     }
-  } else {
+
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
+  } catch (error) {
+    handleInternalError();
   }
 };
